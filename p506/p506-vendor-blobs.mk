3559b7ba704e88d44b2483a6e6946bbc8a06f8b5
Rashed97 <rashedabdeltawab@gmail.com>
2012-07-21 17:10:50 -0300

    Updated to use P506 files

--- p506/p506-vendor-blobs.mk
+++ p506/p506-vendor-blobs.mk
@@ -21,54 +21,54 @@
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ######################    DEPENDENCY SECTION  #######################
 PRODUCT_COPY_FILES += \
-    vendor/lge/p505/proprietary/lib/libril.so:obj/lib/libril.so \
-    vendor/lge/p505/proprietary/lib/libmmipl.so:obj/lib/libmmipl.so \
-    vendor/lge/p505/proprietary/lib/libmmjpeg.so:obj/lib/libmmjpeg.so \
-    vendor/lge/p505/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-    vendor/lge/p505/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
+    vendor/lge/p506/proprietary/lib/libril.so:obj/lib/libril.so \
+    vendor/lge/p506/proprietary/lib/libmmipl.so:obj/lib/libmmipl.so \
+    vendor/lge/p506/proprietary/lib/libmmjpeg.so:obj/lib/libmmjpeg.so \
+    vendor/lge/p506/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/lge/p506/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
 
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ###################### DON'T EDIT ###################################
 PRODUCT_COPY_FILES += \
-    vendor/lge/p505/proprietary/bin/qmuxd:system/bin/qmuxd \
-    vendor/lge/p505/proprietary/etc/flex/flex.xml:system/etc/flex/flex.xml \
-    vendor/lge/p505/proprietary/lib/libgstk_exp.so:system/lib/libgstk_exp.so \
-    vendor/lge/p505/proprietary/lib/libqmi.so:system/lib/libqmi.so \
-    vendor/lge/p505/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
-    vendor/lge/p505/proprietary/lib/libmmgsdilib.so:system/lib/libmmgsdilib.so \
-    vendor/lge/p505/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
-    vendor/lge/p505/proprietary/lib/libdsm.so:system/lib/libdsm.so \
-    vendor/lge/p505/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
-    vendor/lge/p505/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
-    vendor/lge/p505/proprietary/lib/libnv.so:system/lib/libnv.so \
-    vendor/lge/p505/proprietary/lib/libauth.so:system/lib/libauth.so \
-    vendor/lge/p505/proprietary/lib/libril.so:system/lib/libril.so \
-    vendor/lge/p505/proprietary/lib/libwms.so:system/lib/libwms.so \
-    vendor/lge/p505/proprietary/lib/libdiag.so:system/lib/libdiag.so \
-    vendor/lge/p505/proprietary/lib/liboem_rapi.so:system/lib/liboem_rapi.so \
-    vendor/lge/p505/proprietary/lib/libgsdi_exp.so:system/lib/libgsdi_exp.so \
-    vendor/lge/p505/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
-    vendor/lge/p505/proprietary/lib/liblgeat.so:system/lib/liblgeat.so \
-    vendor/lge/p505/proprietary/lib/libcm.so:system/lib/libcm.so \
-    vendor/lge/p505/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-    vendor/lge/p505/proprietary/lib/liblgerft.so:system/lib/liblgerft.so \
-    vendor/lge/p505/proprietary/lib/libdss.so:system/lib/libdss.so \
-    vendor/lge/p505/proprietary/lib/libdll.so:system/lib/libdll.so \
-    vendor/lge/p505/proprietary/lib/libbcmwl.so:system/lib/libbcmwl.so \
-    vendor/lge/p505/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
-    vendor/lge/p505/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
-    vendor/lge/p505/proprietary/lib/libidl.so:system/lib/libidl.so \
-    vendor/lge/p505/proprietary/lib/liblgdrm.so:system/lib/liblgdrm.so \
-    vendor/lge/p505/proprietary/lib/libsnd.so:system/lib/libsnd.so \
-    vendor/lge/p505/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
-    vendor/lge/p505/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/lge/p505/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-    vendor/lge/p505/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-    vendor/lge/p505/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-    vendor/lge/p505/proprietary/etc/firmware/BCM4325D1_004.002.004.0218.0248.hcd:system/etc/firmware/BCM4325D1_004.002.004.0218.0248.hcd \
-    vendor/lge/p505/proprietary/etc/wl/rtecdc.bin:system/etc/wl/rtecdc.bin \
-    vendor/lge/p505/proprietary/etc/wl/rtecdc-apsta.bin:system/etc/wl/rtecdc-apsta.bin \
-    vendor/lge/p505/proprietary/etc/wl/rtecdc-mfgtest.bin:system/etc/wl/rtecdc-mfgtest.bin \
-    vendor/lge/p505/proprietary/bin/ami304d:system/bin/ami304d \
-    vendor/lge/p505/proprietary/lib/hw/sensors.p505.so:system/lib/hw/sensors.p505.so
+    vendor/lge/p506/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/lge/p506/proprietary/etc/flex/flex.xml:system/etc/flex/flex.xml \
+    vendor/lge/p506/proprietary/lib/libgstk_exp.so:system/lib/libgstk_exp.so \
+    vendor/lge/p506/proprietary/lib/libqmi.so:system/lib/libqmi.so \
+    vendor/lge/p506/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
+    vendor/lge/p506/proprietary/lib/libmmgsdilib.so:system/lib/libmmgsdilib.so \
+    vendor/lge/p506/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
+    vendor/lge/p506/proprietary/lib/libdsm.so:system/lib/libdsm.so \
+    vendor/lge/p506/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
+    vendor/lge/p506/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
+    vendor/lge/p506/proprietary/lib/libnv.so:system/lib/libnv.so \
+    vendor/lge/p506/proprietary/lib/libauth.so:system/lib/libauth.so \
+    vendor/lge/p506/proprietary/lib/libril.so:system/lib/libril.so \
+    vendor/lge/p506/proprietary/lib/libwms.so:system/lib/libwms.so \
+    vendor/lge/p506/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+    vendor/lge/p506/proprietary/lib/liboem_rapi.so:system/lib/liboem_rapi.so \
+    vendor/lge/p506/proprietary/lib/libgsdi_exp.so:system/lib/libgsdi_exp.so \
+    vendor/lge/p506/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
+    vendor/lge/p506/proprietary/lib/liblgeat.so:system/lib/liblgeat.so \
+    vendor/lge/p506/proprietary/lib/libcm.so:system/lib/libcm.so \
+    vendor/lge/p506/proprietary/lib/libqueue.so:system/lib/libqueue.so \
+    vendor/lge/p506/proprietary/lib/liblgerft.so:system/lib/liblgerft.so \
+    vendor/lge/p506/proprietary/lib/libdss.so:system/lib/libdss.so \
+    vendor/lge/p506/proprietary/lib/libdll.so:system/lib/libdll.so \
+    vendor/lge/p506/proprietary/lib/libbcmwl.so:system/lib/libbcmwl.so \
+    vendor/lge/p506/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
+    vendor/lge/p506/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+    vendor/lge/p506/proprietary/lib/libidl.so:system/lib/libidl.so \
+    vendor/lge/p506/proprietary/lib/liblgdrm.so:system/lib/liblgdrm.so \
+    vendor/lge/p506/proprietary/lib/libsnd.so:system/lib/libsnd.so \
+    vendor/lge/p506/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
+    vendor/lge/p506/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/lge/p506/proprietary/lib/libcamera.so:system/lib/libcamera.so \
+    vendor/lge/p506/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/lge/p506/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
+    vendor/lge/p506/proprietary/etc/firmware/BCM4325D1_004.002.004.0218.0248.hcd:system/etc/firmware/BCM4325D1_004.002.004.0218.0248.hcd \
+    vendor/lge/p506/proprietary/etc/wl/rtecdc.bin:system/etc/wl/rtecdc.bin \
+    vendor/lge/p506/proprietary/etc/wl/rtecdc-apsta.bin:system/etc/wl/rtecdc-apsta.bin \
+    vendor/lge/p506/proprietary/etc/wl/rtecdc-mfgtest.bin:system/etc/wl/rtecdc-mfgtest.bin \
+    vendor/lge/p506/proprietary/bin/ami304d:system/bin/ami304d \
+    vendor/lge/p506/proprietary/lib/hw/sensors.p505.so:system/lib/hw/sensors.p505.so
